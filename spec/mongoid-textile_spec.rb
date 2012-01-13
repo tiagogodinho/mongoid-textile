113a519bfb0bb84d3874698ed33bdf779b9c8fb4
Lucas Renan <lucas@freelancersbrasil.com>
2012-01-12 22:53:10 -0200

    adds more one test to a better coverage

--- spec/mongoid-textile_spec.rb
+++ spec/mongoid-textile_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require "spec_helper"
 
 class Article
   include Mongoid::Document
@@ -10,13 +10,20 @@ class Article
 end
 
 describe Mongoid::Textile do
-  let(:article) { Article.create(:text => 'h1. proud to be a rails developer') }
+  let(:article) { Article.create(:text => "h1. proud to be a rails developer") }
   
-  it 'should build a dynamic field for textilized fields' do
+  it "should build a dynamic field for textilized fields" do
     article.should respond_to(:text_formatted)
   end
   
-  it 'should set formatted field from textile to html' do
-    article.text_formatted.should eq('<h1>proud to be a rails developer</h1>')
+  it "should set formatted field from textile to html" do
+    article.text_formatted.should eq("<h1>proud to be a rails developer</h1>")
+  end
+
+  it "should change textlized text" do
+    article.text = "p. ruby makes me happy"
+    article.save
+
+    article.text_formatted.should eq("<p>ruby makes me happy</p>")
   end
 end
