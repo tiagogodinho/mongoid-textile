fd0cac4846f233a5c47e71dedeabcfa9b4ff490f
Tiago Rafael Godinho <tiagogodinho3@gmail.com>
2012-06-17 17:38:52 -0300

    Using database_cleaner to clean mongoid database.

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in mongoid-textile.gemspec
 gemspec
+
+gem 'database_cleaner', '~> 0.8'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,5 @@
 require "mongoid-textile"
+require 'database_cleaner'
 
 RSpec.configure do |config|
   # Remove this line if you don"t want RSpec"s should and should_not
@@ -12,18 +13,24 @@ RSpec.configure do |config|
   config.color_enabled 	= true
   config.full_backtrace = true
 
-  config.before :suite do
-  	Mongoid.configure do |config|
-  	  config.master = Mongo::Connection.new.db("mongoid-textile")
-  	end
+  config.before(:suite) do
+    if Mongoid.respond_to? :connect_to
+      Mongoid.connect_to('mongoid-textile')
+    else
+      Mongoid.configure do |config|
+        config.master = Mongo::Connection.new.db('mongoid-textile')
+      end
+    end
+
+    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.clean_with(:truncation)
   end
 
-  config.after :each do
-    Mongoid.master.collections.select {|c| c.name !~ /^system/ }.each(&:drop)
+  config.before(:each) do
+    DatabaseCleaner.start
   end
 
-  config.after :suite do
-    Mongoid.master.connection.drop_database("mongoid-textile")
-    Mongoid.master.connection.close
+  config.after(:each) do
+    DatabaseCleaner.clean
   end
 end
