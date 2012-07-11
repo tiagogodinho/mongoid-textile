44562a86ee2afa9e3e52af3882391fe1d7a3da65
Tiago Rafael Godinho <tiagogodinho3@gmail.com>
2012-07-11 11:48:52 -0300

    Cleaning the spec-helper.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,24 +2,11 @@ require 'mongoid-textile'
 require 'database_cleaner'
 
 RSpec.configure do |config|
-  # Remove this line if you don't want RSpec's should and should_not
-  # methods or matchers
-  require 'rspec/expectations'
-  config.include RSpec::Matchers
-
-  # == Mock Framework
-  config.mock_with :rspec
-
-  config.color_enabled  = true
-  config.full_backtrace = true
-
   config.before(:suite) do
     if Mongoid.respond_to? :connect_to
-      Mongoid.connect_to('mongoid-textile')
+      Mongoid.connect_to('mongoid-textile') # Mongoid 3
     else
-      Mongoid.configure do |config|
-        config.master = Mongo::Connection.new.db('mongoid-textile')
-      end
+      Mongoid.master = Mongo::Connection.new.db('mongoid-textile') # Mongoid 2
     end
 
     DatabaseCleaner.strategy = :truncation
